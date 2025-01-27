37f138a600d471ffdc0ff1fb93b26dd222aed5e3
Sergey Korobok <korvin@kati.ru>
2025-01-27 13:56:19 +0300

    Docker finished

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import {ServerErrorExceptionFilter} from './filters/server-error.filter'
-const cookieParser = require('cookie-parser');
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({ origin: true, credentials: true });
-  app.use(cookieParser());
   //["http://localhost:3006","192.168.50.151"]
   app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true}));
   app.useGlobalFilters(new ServerErrorExceptionFilter());
