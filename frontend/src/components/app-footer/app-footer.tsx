4109aac5d1d239ce81913949f7da0e0e8e295fb3
Sergey Korobok <korvin@kati.ru>
2025-02-11 09:17:00 +0300

    Footer test text

--- frontend/src/components/app-footer/app-footer.tsx
+++ frontend/src/components/app-footer/app-footer.tsx
@@ -6,7 +6,8 @@ import styles from './app-footer.module.css';
 export const AppFooterUI = () =>
     <footer className={styles.footer}>
         <div className={styles.contacts}>
-            <span className={styles.contact_part}>По всем вопросам пишите: <a href="mailto:korvin@sarmat-soft.ru">korvin@sarmat-soft.ru</a></span>
-            <span className={styles.contact_part}>или стучитесь в Telegram: <a href="https://t.me/KorvinTag">@KorvinTag</a></span>
+            <span className={styles.contact_part}>По всем вопросам пишите: <a href="mailto:korvin@sarmat-soft.ru">korvin@sarmat-soft.ru</a> </span>
+            <span className={styles.contact_part}>{'\u00A0'}или стучитесь в Telegram: <a href="https://t.me/KorvinTag">@KorvinTag</a></span>
         </div>
+        <div className={styles.contact_part}>Ресурс находится в стадии тестирования. Данные не сохраняются!</div>
     </footer>
