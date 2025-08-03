4787cb0646e1d7b48b58288b735543cf13337983
Sergey Korobok <korvin@kati.ru>
2025-08-03 16:16:09 +0300

    feat: telegram registration admin notify

--- backend/src/DDD/telegram/registration/telegram.service.ts
+++ backend/src/DDD/telegram/registration/telegram.service.ts
@@ -4,20 +4,26 @@ import { plainToInstance } from 'class-transformer';
 import { TelegramSessions } from './entities/telegram-sessions.entity';
 import { Repository, ILike } from 'typeorm';
 import { CreateTelegramSessionsDto } from './dto/create-telegram-sessions.dto';
+import { TelegramMessagingService } from '../messages/telegram-messages.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TelegramSessionsService {
   constructor(
     @InjectRepository(TelegramSessions)
     private readonly telegramSessionsRepository: Repository<TelegramSessions>,
+    private readonly messageService: TelegramMessagingService,
+    private configService: ConfigService,
   ) {}
 
   private _genUser(user: Partial<TelegramSessions>) {
     return plainToInstance(TelegramSessions, user);
   }
 
-  create(createUserDto: CreateTelegramSessionsDto): Promise<TelegramSessions> {
-    return this.saveUser(createUserDto);
+  async create(createUserDto: CreateTelegramSessionsDto): Promise<TelegramSessions> {
+    const res = await this.saveUser(createUserDto);
+    this.messageService.sendMessage(this.configService.get('SUPERADMIN_USER_ID'), `Добавлен новый пользователь chat_id=${res.chat_id} с id ${res.id}`);
+    return res;
   }
 
   saveUser(user: Partial<TelegramSessions>) {
